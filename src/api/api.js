f2210992d94b40bf3f906c1bb29ddeb1968ce396
Nirmallya Dey <deysagar074@gmail.com>
2023-12-27 17:47:36 +0530

    api updated

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,6 @@
 // Import any necessary modules or libraries
 
 // Define your API endpoint
-export const apiUrl = 'https://backendapi-wtkn.onrender.com';
+export const apiUrl = 'https://api.render.com/deploy/srv-cm60547qd2ns73eqrn1g?key=sMnQPJxeYAw';
 
 
