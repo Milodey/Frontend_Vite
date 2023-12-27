f662714f926b928bd30e5c143264efdc94bef52c
Nirmallya Dey <deysagar074@gmail.com>
2023-12-27 16:29:32 +0530

    api connected

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,6 @@
 // Import any necessary modules or libraries
 
 // Define your API endpoint
-export const apiUrl = 'http://localhost:3000';
+export const apiUrl = 'https://backendapi-wtkn.onrender.com';
 
 
