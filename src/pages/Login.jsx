84e4bcb4b53366df3ec58011d7f1be5974f35d17
Milodey <deysagar074@gmail.com>
2024-01-01 13:19:59 +0530

    rewind

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
 
 
     try {
-      const response = await axios.post(`/api/login`, {
+      const response = await axios.post(`${apiUrl}/login`, {
         email,
         password,
       }, {
